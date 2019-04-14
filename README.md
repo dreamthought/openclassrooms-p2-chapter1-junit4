921df7d7f2e89188fba243db65b9848eb1630cdb
Raf Gemmail <rafiq@dreamthought.com>
2019-04-14 15:52:16 +1200

    Prepared tests for a student to mess around with

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 
 * You can run the tests with `mvn clean test`
 * These examples are fully described in my Openclassrooms course on testing in Java.
-* Run just the conversion tests with `mvn -Dgroups='com.openclassrooms.testing.Categories$ConversionTests' test`
+
+Here are some things for your to try out:
+* Run just the conversion tests with
+ `mvn -Dgroups='com.openclassrooms.testing.Categories$ConversionTests' test`
 * *Don't forget the quotes around the class name or the $ might mean something completely different on your command line*!
+* We have a failng test which needs to be @Ignored'd - this skips that test until you're ready to fix it.
 * *Have a look at the incomplete `convert` method in  `Calculator.java`. Can you TDD converting Celsius to Fahrenheit?
--- src/test/java/com/openclassrooms/testing/ConversionCalculatorTest.java
+++ src/test/java/com/openclassrooms/testing/ConversionCalculatorTest.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.testing;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -7,6 +8,7 @@ import static java.lang.Math.PI;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 @Category(Categories.ConversionTests.class)
 public class ConversionCalculatorTest {
@@ -16,27 +18,34 @@ public class ConversionCalculatorTest {
 
     @Test
     @Category({Categories.FahrenheitTests.class, Categories.TemperatureTests.class})
-    public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsPositive() {
+    public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsZero() {
         Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(0.0 );
         assertThat( actualFahrenheit, is(equalTo(32.0)) );
     }
 
     @Test
     @Category({Categories.TemperatureTests.class})
-    public void fahrenheitToCelsius() {
+    public void fahrenheitToCelsius_returnsZeroCelciusTempurature_whenThirtyTwo() {
         Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(32.0);
         assertThat( actualCelsius, is(equalTo(0.0)));
     }
 
     @Test
-    public void litresToGallons() {
+    public void litresToGallons_returnsOneGallon_whenConvertingTheEquivalentLitres() {
         Double actualLitres = calculatorUnderTest.litresToGallons(3.78541);
         assertThat( actualLitres, is(equalTo(1.0)));
     }
 
     @Test
-    public void radiusToAreaOfCircle() {
+    public void radiusToAreaOfCircle_returnsPi_whenWeHaveARadiusOfOne() {
         Double actualArea = calculatorUnderTest.radiusToAreaOfCircle(1.0);
         assertThat( actualArea, is(equalTo(PI)));
+    }
+
+    //@Ignore("Meters to KM disabled as this has not been written")
+    @Test
+    public void metersToKiloMeters_returns1Km_whenWeConvertAHundredMeters() {
+        Double expected = 1.0;
+        fail("Not written yet");
     }
 }
