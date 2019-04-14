a49e34c62c9290e9049debebaf3c4de997a397ff
Raf Gemmail <rafiq@dreamthought.com>
2019-04-14 14:49:23 +1200

    Added different types of conversions

---
+++ src/main/java/com/openclassrooms/testing/ConversionType.java
@@ -0,0 +1,11 @@
+package com.openclassrooms.testing;
+
+/**
+ * Types of conversions supports.
+ */
+public enum ConversionType {
+    FARHANHEIT_TO_CELSIUS,
+    CELSIUS_TO_FARHANHEIT,
+    LITRES_TO_GALLONS,
+    RADIUS_TO_AREA
+}
